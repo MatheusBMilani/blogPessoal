a457590b27dfaaa825a50a4d502978b67829bc09
Matheus de Brito Milani <matheusdebritomilani@gmail.com>
2021-09-09 10:05:54 -0300

    Add files via upload

---
+++ src/main/java/org/example/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,21 @@
+package org.example.blogPessoal.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.example.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public Optional<Usuario> findByUsuario(String usuario);
+
+	// Usaremos na Sessão de Testes
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+	// Usaremos na Sessão de Testes
+	public Usuario findByNome(String nome);
+
+}
